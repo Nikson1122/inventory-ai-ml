c585b226832cf5d413e10eb028cf1fa175894ed4
Nikson1122 <104704390+Nikson1122@users.noreply.github.com>
2025-10-28 17:09:20 +0545

    using random forest the data is trained initally

--- train_model.py
+++ train_model.py
@@ -7,11 +7,22 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 from catboost import CatBoostClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 import warnings
 warnings.filterwarnings('ignore')
 
 file_path = "retail_store_inventory.csv"
 df = pd.read_csv(file_path)
+
+df['Date'] = pd.to_datetime(df['Date'])
+df['Year'] = df['Date'].dt.year
+df['Month'] = df['Date'].dt.month
+df['Day'] = df['Date'].dt.day
+df['Weekday'] = df['Date'].dt.weekday
 
 print("First five rows of the dataset:")
 print(df.head())
@@ -29,8 +40,28 @@ for col in df.columns:
     print(f"\nColoumn: {col}")
     print(df[col].unique() [:10])
 
+X= df.drop("Demand Forecast", axis=1)
+X = X.drop("Date", axis=1)
+y = df["Demand Forecast"]
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+categorical_cols = ['Store ID', 'Product ID', 'Category', 'Region', 'Weather Condition', 'Seasonality']
+numerical_cols = X.select_dtypes(include=['int64','float64']).columns.tolist()
 
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_cols)
+    ], remainder='passthrough'
+)
 
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('regressor', RandomForestRegressor(n_estimators=100, random_state=42))
+])
 
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
+from sklearn.metrics import mean_squared_error, r2_score
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
